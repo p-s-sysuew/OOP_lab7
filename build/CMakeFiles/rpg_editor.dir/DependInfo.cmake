4b97caa4f677c71af7b8daa7d60d900e9617a977
pavel <pavelsysuev20@gmail.com>
2025-12-18 03:34:50 +0300

    1

---
+++ build/CMakeFiles/rpg_editor.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/pavel/MyFiles/OOP_labs/OOP_lab7/functions.cpp" "CMakeFiles/rpg_editor.dir/functions.cpp.o" "gcc" "CMakeFiles/rpg_editor.dir/functions.cpp.o.d"
+  "/home/pavel/MyFiles/OOP_labs/OOP_lab7/main.cpp" "CMakeFiles/rpg_editor.dir/main.cpp.o" "gcc" "CMakeFiles/rpg_editor.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/rpg_editor.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rpg_editor.dir/functions.cpp.o"
+  "CMakeFiles/rpg_editor.dir/functions.cpp.o.d"
+  "CMakeFiles/rpg_editor.dir/main.cpp.o"
+  "CMakeFiles/rpg_editor.dir/main.cpp.o.d"
+  "rpg_editor"
+  "rpg_editor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rpg_editor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
